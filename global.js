4f4a367487748044ed654fec8df8f90bc51f883e
randesal <raesalillas@gmail.com>
2023-06-12 22:43:46 +0800

    added modules

--- global.js
+++ global.js
@@ -1,14 +1,15 @@
 //create 
 //console.log(global);
 
-global.setTimeout(() => {
+/* global.setTimeout(() => {
     console.log('adrian');
     clearInterval(int);
-}, 3000);
+}, 3000); */
 
-const int = setInterval(() => {
+/* const int = setInterval(() => {
     console.log('adrian2')
-}, 1000);
+}, 1000); */
 
-console.log(__dirname);
-console.log(__filename);
+/* console.log(__dirname);
+console.log(__filename); */
+
---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['randi','adrian','empio','salillas'];
+const ages = [12,13,14,15];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+}
