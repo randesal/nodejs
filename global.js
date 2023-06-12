c97c544deb22782422c738fa7ef19676425bb506
randesal <raesalillas@gmail.com>
2023-06-12 22:25:09 +0800

    create new files

---
+++ global.js
@@ -0,0 +1,14 @@
+//create 
+//console.log(global);
+
+global.setTimeout(() => {
+    console.log('adrian');
+    clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+    console.log('adrian2')
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
